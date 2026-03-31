2b9b28c2fe714afe864670d7257d69dad5ed88d8
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-03-30 23:23:44 -0300

    Update transcript.py

--- utils/transcript.py
+++ utils/transcript.py
@@ -1,1 +1,15 @@
+import discord
 
+async def create_transcript(channel):
+    html = "<html><body>"
+
+    async for msg in channel.history(limit=None, oldest_first=True):
+        html += f"<p><b>{msg.author}:</b> {msg.content}</p>"
+
+    html += "</body></html>"
+
+    file = f"{channel.name}.html"
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(html)
+
+    return file
