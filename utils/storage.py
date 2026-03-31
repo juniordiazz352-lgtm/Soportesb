25e97e055dca35775105789048ca0b1a00bbfbda
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-03-30 22:58:52 -0300

    Update storage.py

--- utils/storage.py
+++ utils/storage.py
@@ -1,1 +1,12 @@
+import json, os
 
+def load_data(path):
+    if not os.path.exists(path):
+        with open(path, "w") as f:
+            json.dump({}, f)
+    with open(path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def save_data(path, data):
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
