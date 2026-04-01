107afb3ef165bffc772eb719ddce511f3d0d1336
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-03-31 21:40:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,37 +2,36 @@ import discord
 from discord.ext import commands
 import asyncio
 import config
+
 import threading
 from flask import Flask
+import os
 
+# 🌐 Render fix
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Bot activo"
 
-def run_web():
-    app.run(host="0.0.0.0", port=10000)
+def run():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
-threading.Thread(target=run_web).start()
+threading.Thread(target=run).start()
 
+# 🤖 Bot
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"🔥 {bot.user} listo")
-
-    try:
-        synced = await bot.tree.sync()
-        print(f"Comandos: {len(synced)}")
-    except Exception as e:
-        print(e)
+    await bot.tree.sync()
 
 async def main():
     async with bot:
-        await bot.load_extension("cogs.forms")
         await bot.load_extension("cogs.tickets")
+        await bot.load_extension("cogs.forms")
         await bot.start(config.TOKEN)
 
 asyncio.run(main())
