1511bf917b48d5e0c6f0182c9081bee0bbb831c5
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-01 23:49:19 -0300

    Update setup.py

--- comands/setup.py
+++ comands/setup.py
@@ -15,3 +15,31 @@ class Setup(commands.Cog):
 
 async def setup(bot):
     await bot.add_cog(Setup(bot))
+
+
+from discord.ext import commands
+from discord import app_commands
+from database.db import load, save
+
+GUILDS_FILE = "database/guilds.json"
+
+class Setup(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="setup")
+    async def setup(self, interaction, staff_role: discord.Role, category: discord.CategoryChannel, logs: discord.TextChannel):
+        data = load(GUILDS_FILE)
+
+        data[str(interaction.guild.id)] = {
+            "staff_role": staff_role.id,
+            "category": category.id,
+            "logs": logs.id
+        }
+
+        save(GUILDS_FILE, data)
+
+        await interaction.response.send_message("✅ Configurado", ephemeral=True)
+
+async def setup(bot):
+    await bot.add_cog(Setup(bot))
