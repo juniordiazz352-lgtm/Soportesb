8783be8459427e9446b044053a790d73e059f9bb
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-02 20:18:40 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,54 +1,35 @@
+
 import discord
 from discord.ext import commands
-import asyncio
-from config import TOKEN
 from view.panel_view import PanelView
-from view.ticket_controls import TicketControls
-from view.form_dynamic_panel import FormPanel
-import sys
-import os
-from dotenv import load_dotenv
+from utils.embeds import panel_embed
 import os
 
-load_dotenv()
-
-TOKEN = os.getenv("TOKEN")
-
-if not TOKEN:
-    raise Exception("❌ TOKEN no encontrado")
-
-bot.run(TOKEN)
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# 👇 FORZAR RUTA CORRECTA
-sys.path.insert(0, BASE_DIR)
-sys.path.insert(0, os.path.join(BASE_DIR))
-
-print("📂 PATH DEBUG:", sys.path)
-print("📂 ARCHIVOS:", os.listdir(BASE_DIR))
-sys.path.insert(0, BASE_DIR)
-bot.add_view(FormPanel(guild_id=0))
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+TOKEN = os.getenv("TOKEN")
+
 @bot.event
 async def on_ready():
-    print(f"🔥 Startup Bot listo: {bot.user}")
+    print(f"✅ Bot listo como {bot.user}")
 
-    bot.add_view(PanelView())
-    bot.add_view(TicketControls(user_id=0))
-    
-    await bot.tree.sync()
 
-@bot.event
-async def on_command_error(ctx, error):
-    print("ERROR:", error)
+# 🔥 CREAR PANEL MULTI BOTONES
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def panel_multi(ctx, *, opciones):
+    tipos = [t.strip() for t in opciones.split(",")]
 
-async def main():
-    async with bot:
-        await bot.load_extension("comands.forms")
-        await bot.load_extension("comands.setup")
-        await bot.start(TOKEN)
+    embed = panel_embed(
+        "🎟️ Sistema de Tickets",
+        "Selecciona una opción:"
+    )
 
-asyncio.run(main())
+    await ctx.send(
+        embed=embed,
+        view=PanelView(tipos)
+    )
+
+
+bot.run(TOKEN)
