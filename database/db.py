e481f4957707e4118b18d6b7cb52111273d82416
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-01 23:46:06 -0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,12 @@
+import json
+import os
 
+def load(path):
+    if not os.path.exists(path):
+        return {}
+    with open(path, "r") as f:
+        return json.load(f)
+
+def save(path, data):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
