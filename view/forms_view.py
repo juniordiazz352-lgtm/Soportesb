344ce37c4544431dfe7721abe7bc755ab3cbedb1
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-01 23:57:44 -0300

    Update and rename form_view.py to forms_view.py

---
+++ view/forms_view.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ui import View, Select
+from systems.form_system import get_forms, DynamicForm
+
+class FormPanel(View):
+    def __init__(self, guild_id):
+        super().__init__(timeout=None)
+
+        forms = get_forms(guild_id)
+
+        options = [
+            discord.SelectOption(label=name)
+            for name in forms.keys()
+        ]
+
+        if options:
+            self.add_item(FormSelect(options, forms))
+
+
+class FormSelect(Select):
+    def __init__(self, options, forms):
+        super().__init__(placeholder="Selecciona formulario...", options=options)
+        self.forms = forms
+
+    async def callback(self, interaction):
+        form = self.forms[self.values[0]]
+
+        await interaction.response.send_modal(
+            DynamicForm(
+                self.values[0],
+                form["preguntas"],
+                form["canal"]
+            )
+        )
