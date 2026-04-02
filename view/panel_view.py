763813bf2283f13bcf10e0ef4a8c48bac7bad2cd
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-02 20:19:13 -0300

    Update panel_view.py

--- view/panel_view.py
+++ view/panel_view.py
@@ -1,20 +1,16 @@
 import discord
-from discord.ui import View, Select
+from discord.ui import View, Button
 from system.ticket_system import create_ticket
 
 class PanelView(View):
-    def __init__(self):
+    def __init__(self, tipos):
         super().__init__(timeout=None)
-        self.add_item(TicketSelect())
 
-class TicketSelect(Select):
-    def __init__(self):
-        options = [
-            discord.SelectOption(label="Soporte", emoji="🛠", description="Ayuda general"),
-            discord.SelectOption(label="Compras", emoji="💰", description="Pagos y compras"),
-            discord.SelectOption(label="Reportes", emoji="⚠", description="Reportar usuarios"),
-        ]
-        super().__init__(placeholder="Selecciona una categoría...", options=options)
+        for tipo in tipos:
+            button = Button(label=tipo, style=discord.ButtonStyle.primary)
 
-    async def callback(self, interaction: discord.Interaction):
-        await create_ticket(interaction, self.values[0])
+            async def callback(interaction: discord.Interaction, t=tipo):
+                await create_ticket(interaction.guild, interaction.user, t)
+
+            button.callback = callback
+            self.add_item(button)
