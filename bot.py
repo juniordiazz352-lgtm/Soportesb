4a45fe856ddede3243b4922c9a6dd4109f2a8997
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-01 23:49:36 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,21 @@
 import discord
 from discord.ext import commands
 import asyncio
-from config import TOKEN, GUILD_ID
+from config import TOKEN
 from views.panel_view import PanelView
 from views.ticket_controls import TicketControls
 
 intents = discord.Intents.all()
-
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"✅ Conectado como {bot.user}")
+    print(f"🔥 Startup Bot listo: {bot.user}")
 
-    # Persistencia de botones
     bot.add_view(PanelView())
     bot.add_view(TicketControls(user_id=0))
 
-    await bot.tree.sync(guild=discord.Object(id=GUILD_ID))
+    await bot.tree.sync()
 
 async def main():
     async with bot:
