92031285390e3cd2cbde29f0aae0ec2cac4f9ce9
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-02 21:38:42 -0300

    Update form_panel.py

--- view/form_panel.py
+++ view/form_panel.py
@@ -3,15 +3,15 @@ from discord.ui import View, Button
 from view.dynamic_form_modal import DynamicForm
 
 class FormPanel(View):
-    def __init__(self, guild_id, form_name):
+    def __init__(self, guild_id, name):
         super().__init__(timeout=None)
 
-        button = Button(label=form_name, style=discord.ButtonStyle.success)
+        btn = Button(label=name)
 
-        async def callback(interaction: discord.Interaction):
+        async def callback(interaction):
             await interaction.response.send_modal(
-                DynamicForm(guild_id, form_name)
+                DynamicForm(guild_id, name)
             )
 
-        button.callback = callback
-        self.add_item(button)
+        btn.callback = callback
+        self.add_item(btn)
