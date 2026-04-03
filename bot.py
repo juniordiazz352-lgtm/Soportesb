09a3d5257741b0a56af3e24ba46b60c8a62f9180
juniordiazz352-lgtm <juniordiazz.352@gmail.com>
2026-04-02 21:42:01 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
-
 import discord
 from discord.ext import commands
-from view.panel_view import PanelView
-from utils.embeds import panel_embed
 import os
+
+from view.panel_view import PanelView
+from view.form_panel import FormPanel
+from utils.embeds import panel_embed
+from system.form_system import create_form
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -12,24 +14,25 @@ TOKEN = os.getenv("TOKEN")
 
 @bot.event
 async def on_ready():
-    print(f"✅ Bot listo como {bot.user}")
+    print("Bot listo")
 
 
-# 🔥 CREAR PANEL MULTI BOTONES
 @bot.command()
-@commands.has_permissions(administrator=True)
 async def panel_multi(ctx, *, opciones):
     tipos = [t.strip() for t in opciones.split(",")]
+    await ctx.send(embed=panel_embed("Tickets", "Elige"), view=PanelView(tipos))
 
-    embed = panel_embed(
-        "🎟️ Sistema de Tickets",
-        "Selecciona una opción:"
-    )
 
-    await ctx.send(
-        embed=embed,
-        view=PanelView(tipos)
-    )
+@bot.command()
+async def crear_form(ctx, nombre: str, canal: discord.TextChannel, *, preguntas):
+    preguntas_lista = [p.strip() for p in preguntas.split("|")]
+    create_form(ctx.guild.id, nombre, preguntas_lista, canal.id)
+    await ctx.send("Formulario creado")
+
+
+@bot.command()
+async def panel_form(ctx, nombre: str):
+    await ctx.send("Formulario", view=FormPanel(ctx.guild.id, nombre))
 
 
 bot.run(TOKEN)
